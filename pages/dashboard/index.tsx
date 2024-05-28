0052ed763c05b38e8209429cd94f8bd49fef123f
Samuel Goncalves <samuelfernandogoncalves@gmail.com>
2024-05-28 12:16:14 -0300

    auto push

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -5,6 +5,7 @@ import { GetServerSideProps, InferGetServerSidePropsType } from 'next/types';
 import { useServices } from 'shared/hooks/useServices';
 import { PageProps } from 'shared/types/PagePropsType';
 import { LocationModel, UserModel } from 'shared/types/api-type';
+import { useEffect, useState } from 'react';
 
 const StartPage = dynamic<PageProps>(
   () =>
@@ -15,59 +16,14 @@ const StartPage = dynamic<PageProps>(
 );
 
 export const getServerSideProps = (async ({ req, res }) => {
-  const { authenticate, updateUser, getLocationByName, getClientIp } =
-    useServices();
+  const { authenticate } = useServices();
 
   const authToken = getCookie('authToken', { req, res });
   const authResponse = await authenticate({
     authToken
   });
 
-  //console.log('authResponse.data', authResponse.data);
-
-  //let clientIp = await getIp(req as NextApiRequest);
-
-  //console.log('clientIp', clientIp);
-
-  const ipLocationResponse = await getClientIp();
-  const ipLocation = ipLocationResponse.data;
-  console.log('ipLocation', ipLocation);
-  if (ipLocation.error) {
-    //TODO: get from browser country
-  }
-  const locationResponse = await getLocationByName({
-    name: ipLocation.city,
-    longitude: ipLocation.longitude.toString(),
-    latitude: ipLocation.latitude.toString()
-  });
-
-  let location = locationResponse.data;
-
-  if (!location) {
-    //TODO: ERROR could not find the city on public.opendatasoft.com imported database
-    //with the ip data coming from https://ipapi.co/
-    //(maybe raising the range in km it works??)
-    //to workaround with it i'm building manually the opendatasoft object
-    console.error(
-      'ERROR could not find the city on public.opendatasoft.com by imported database'
-    );
-    location = {
-      name: ipLocation.city,
-      country: ipLocation.country_name,
-      longitude: ipLocation.longitude.toString(),
-      latitude: ipLocation.latitude.toString(),
-      coordinates: { lon: ipLocation.longitude, lat: ipLocation.latitude }
-    } as LocationModel;
-  }
-
-  const currentUser = { ...authResponse.data, location };
-
-  await updateUser(
-    {
-      locationGeonameId: location.geoname_id
-    } as UserModel,
-    authToken
-  );
+  const currentUser = authResponse.data;
 
   if (!currentUser) {
     return {
@@ -84,11 +40,65 @@ export const getServerSideProps = (async ({ req, res }) => {
 const Page = ({
   currentUser
 }: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  const { updateUser, getLocationByName, getClientIp } = useServices();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const getUserLocation = async () => {
+    setLoading(true);
+    const ipLocationResponse = await getClientIp();
+    const ipLocation = ipLocationResponse.data;
+    console.log('ipLocation', ipLocation);
+    if (ipLocation.error) {
+      //TODO: get from browser country
+    }
+    const locationResponse = await getLocationByName({
+      name: ipLocation.city,
+      longitude: ipLocation.longitude.toString(),
+      latitude: ipLocation.latitude.toString()
+    });
+
+    let location = locationResponse.data;
+
+    if (!location) {
+      //TODO: ERROR could not find the city on public.opendatasoft.com imported database
+      //with the ip data coming from https://ipapi.co/
+      //(maybe raising the range in km it works??)
+      //to workaround with it i'm building manually the opendatasoft object
+      console.error(
+        'ERROR could not find the city "' +
+          ipLocation.city +
+          '" on locations table'
+      );
+      location = {
+        name: ipLocation.city,
+        country: ipLocation.country_name,
+        longitude: ipLocation.longitude.toString(),
+        latitude: ipLocation.latitude.toString(),
+        coordinates: { lon: ipLocation.longitude, lat: ipLocation.latitude }
+      } as LocationModel;
+    }
+
+    currentUser = { ...currentUser, location };
+
+    await updateUser(
+      {
+        locationGeonameId: location.geoname_id
+      } as UserModel,
+      currentUser.authToken
+    );
+
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    getUserLocation();
+  }, []);
+
   return (
     <>
       <link rel="stylesheet" href="/static/externals/simplemde.min.css" />
       <script src="/static/externals/simplemde.min.js" defer></script>
-      <StartPage currentUser={currentUser} />
+      {loading ? <></> : <StartPage currentUser={currentUser} />}
     </>
   );
 };
--- src/shared/hooks/useLocation.ts
+++ src/shared/hooks/useLocation.ts
@@ -1,86 +1,17 @@
 import { NextApiRequest } from 'next';
-import { IPLocation, LocationModel } from 'shared/types/api-type';
+import { LocationModel } from 'shared/types/api-type';
 
 export type LocationProps = {
   getIp: (req: NextApiRequest) => string;
   getDistanceBetweenCitites: (from: LocationModel, to: LocationModel) => number;
 };
 
-/*
-const OPEN_DATASOFT_URI =
-  'https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-500/records';
-*/
 export const useLocation = (): LocationProps => {
-  //https://ipapi.co/8.8.8.8/json/
-  //https://ipapi.co/8.8.8.8/json/?key=jNXKxf32P1D6X4lsYh7uTLR7JEPxofsHCF8SoSBHB8TzguGrO3
-  //https://public.opendatasoft.com/api/explore/v2.1/catalog/datasets/geonames-all-cities-with-a-population-500/records?where=name%20like%20%27astorga%27&limit=20
-
   const getIp = (req: NextApiRequest) => {
     let ipAddress = req.headers['x-forwarded-for'];
     console.log(ipAddress, req.connection.remoteAddress);
     return (ipAddress as string).split(',').pop() as string;
   };
-
-  /*const getCitiesNearby = async (
-    lon: string,
-    lat: string,
-    distanceInKm: number
-  ) => {
-    let response: LocationResponse;
-
-    const rawResponse = await fetch(
-      `${OPEN_DATASOFT_URI}?where=${encodeURIComponent(
-        `within_distance(coordinates, GEOM'POINT(${lon} ${lat})', ${distanceInKm}km)`
-      )}`
-    );
-
-    response = (await rawResponse.json()) as LocationResponse;
-    //console.log('getCitiesAround()', response);
-    return response;
-  };*/
-
-  /*const getCityByNameAndLocation = async (
-    name: string,
-    lon: string,
-    lat: string
-  ) => {
-    let response: LocationResponse;
-
-    const rawResponse = await fetch(
-      `${OPEN_DATASOFT_URI}?where=${encodeURIComponent(
-        `name = '${name}' AND within_distance(coordinates, GEOM'POINT(${lon} ${lat})', 50km)`
-      )}`
-    );
-
-    response = (await rawResponse.json()) as LocationResponse;
-    //console.log(`getCityByNameAndLocation(${name})`, response);
-    return response?.results.length && response?.results[0];
-  };*/
-
-  /*const getCurrentLocation = async (ip: string): Promise<IPLocation> => {
-    let location: IPLocation;
-
-    const rawResponse = await fetch(
-      process.env.NODE_ENV == 'production'
-        ? `${IPAPI_URI}/${ip}/json?key=${IPAPI_KEY}`
-        : `${IPAPI_URI}/json?key=${IPAPI_KEY}`
-    );
-    location = (await rawResponse.json()) as IPLocation;
-
-    return location;
-  };*/
-
-  /*const getCitiesByName = async (name: string): Promise<LocationResponse> => {
-    let response: LocationResponse;
-    const rawResponse = await fetch(
-      `${OPEN_DATASOFT_URI}?where=${encodeURIComponent(
-        `name like '${name}*'`
-      )}&limit=100`
-    );
-    response = (await rawResponse.json()) as LocationResponse;
-    //console.log('getCitiesByName(' + name + ')', response);
-    return response;
-  };*/
 
   const getDistanceBetweenCitites = (
     from: LocationModel,
